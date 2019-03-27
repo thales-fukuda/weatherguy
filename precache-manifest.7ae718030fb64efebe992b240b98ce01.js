fe11e379fe61c0107213e2478a7579edfe55f019
thales-fukuda <thalesc@alunos.utfpr.edu.br>
2019-03-26 23:39:56 -0300

    Updates

---
+++ precache-manifest.7ae718030fb64efebe992b240b98ce01.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/weatherguy/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/weatherguy/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/weatherguy/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/weatherguy/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "dc98c4e3a65836bb7752",
+    "url": "/weatherguy/static/js/runtime~main.dc98c4e3.js"
+  },
+  {
+    "revision": "52c99e10add974c3accb",
+    "url": "/weatherguy/static/js/main.52c99e10.chunk.js"
+  },
+  {
+    "revision": "99f6a9a9518bd7b5951c",
+    "url": "/weatherguy/static/js/1.99f6a9a9.chunk.js"
+  },
+  {
+    "revision": "99f6a9a9518bd7b5951c",
+    "url": "/weatherguy/static/css/1.42b08c39.chunk.css"
+  },
+  {
+    "revision": "c146812d7506ea55c4c86762c86083dc",
+    "url": "/weatherguy/index.html"
+  }
+];
